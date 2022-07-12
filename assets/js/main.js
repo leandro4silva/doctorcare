793c66dd53cdbb4bc86d516b43ad5f4041d58419
leandro4silva <leandro94.a.s@gmail.com>
2022-07-11 22:31:07 -0300

    Doctor Care

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,3 +7,21 @@ function onScroll(){
         nav.classList.add('scroll')
     }
 }
+
+function openMenu(){
+    document.body.classList.add('menu-expanded')
+}
+
+function closeMenu(){
+    document.body.classList.remove('menu-expanded')
+}
+
+ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 700,
+}).reveal(`#home, #home img, #home .stats, 
+           #services, #services header, #services service-item,
+           #about, #about img,
+           #contact, #contact img,
+           footer`);
